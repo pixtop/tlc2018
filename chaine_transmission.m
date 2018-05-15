548751449de3df113a1a77763bb0ed1f5d3a5062
PixTop <pixtopmc@gmail.com>
2018-05-15 13:39:30 +0200

    final

--- chaine_transmission.m
+++ chaine_transmission.m
@@ -1,72 +1,115 @@
-function [ x, xp, z ] = chaine_transmission( bits, Ts, filtre, SNR, bruit, fp )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
+function [ x,  z ] = chaine_transmission( bits, Ts, filtre, SNR, bruit, fp, modu )
+% Cette fonction modelise la chaine de transmission de l'emission jusqu'a
+% la reception.
+%   Parametres :
+%    - bits   | la chaine de bits a transmettre
+%    - Ts     | le nombre d'echantillon par symbole
+%    - filtre | ('RA','RNA','FA','CA') filtre Forme+Reception
+%    - SNR    | la valeur du signal sur bruit a appliquer
+%    - bruit  | ('none','real','complex') type de bruit dans le canal
+%    - fp     | Valeur de la fréquence porteuse, 0+QPSK = passe-bas
+%    - modu   | (BPSK,QPSK,8PSK,16QAM)  modulation sur frq porteuse
+%
+%   Retours :
+%    - x  | valeurs du signal mis en forme (complexe si QPSK,8PSK,16QAM)
+%    - z  | valeurs du signal recu
+%--------------------------------------------------------------------------
 %% Type de filtre
 
 switch (filtre)
-    case 1
+    case 'RA'
         h = ones(1,Ts);
         hr = h;
-    case 2
+    case 'RNA'
         h = ones(1,Ts);
         hr = kron([1 0], ones(1,Ts/2));
-    case 3
+    case 'FA'
         h = [-1*ones(1,Ts/4) ones(1,Ts/2) -1*ones(1,Ts/4)];
         hr = h;
-    case 4
+    case 'CA'
         span = 10;
         h = rcosdesign(0.5,span,Ts);
         hr = h;
 end
 
-%% Diracs
+%% Symboles
 
 symboles = 2*bits-1;
 
-if fp
-    symboles = symboles(1:2:end)+1i*symboles(2:2:end);
+%% Type de modulation
+
+switch (modu)
+    case 'BPSK'
+        M = 2;
+        sigma_a2 = 1;
+    case 'QPSK'
+        M = 4;
+        sigma_a2 = 2;
+        symboles = symboles(1:2:end)+1i*symboles(2:2:end);
+    case '8PSK'
+        M = 8;
+        sigma_a2 = 1;
+        bits1 = reshape(bits,log2(M),[]);
+        bits2 = bi2de(bits1');
+        symboles = pskmod(bits2', M, 0,'gray');
+    case '16QAM'
+        M = 16;
+        sigma_a2 = 10;
+        bits1 = reshape(bits,log2(M) ,[]);
+        bits2 = bi2de(bits1');
+        symboles = qammod(bits2', M,'gray');
 end
+
+%% Diracs
 
 diracs = kron(symboles, [1 zeros(1,Ts-1)]);
 
-%% Emission
-
-if filtre == 4
-    diracs = [diracs zeros(1,span*Ts)];
+if strcmp(filtre,'CA')
+    diracs = [diracs zeros(1,span*Ts)]; %Compensation du retard du filtre
 end
+
+%% Filtrage de mise en forme
 
 x = filter(h,1,diracs);
 
-%% Emission frequence porteuse
+%% Transposition sur frequence porteuse
 
-if fp
+if fp ~= 0 && strcmp(modu,'QPSK')
    xp = x.*exp(1i*2*pi*fp*(1:length(x)));
    xp = real(xp);
 end
 
 %% Bruit AWGN
 
-if bruit
-    sigma_a2 = 1;
-    M = 2;
-    sigma_n2 = sigma_a2*(h*h')/(2*log2(M)*SNR);
+if  ~strcmp(bruit,'none')
+    if fp ~= 0 && strcmp(modu,'QPSK')
+        sigma_n2 = sigma_a2*(h*h')/(4*log2(M)*SNR);
+    else
+        sigma_n2 = sigma_a2*(h*h')/(2*log2(M)*SNR);
+    end
     n = randn(1,length(diracs))*sqrt(sigma_n2);
+    if strcmp(bruit,'complex')
+        n = n + 1i*randn(1,length(diracs))*sqrt(sigma_n2);  %Bruit complexe
+    end
 else
     n = 0;
 end
 
 %% Canal de transmission
 
-r = x + n;
-
-%% Reception
-
-if fp
-   rc = r.*exp(1i*2*pi*fp*(1:length(x)));
+if fp ~= 0 && strcmp(modu,'QPSK')
+    r = xp + n;
+else
+	r = x + n;
 end
 
-z = filter(hr,1,rc);
+%% Filtrage de reception
+
+if fp ~= 0 && strcmp(modu,'QPSK')
+   r = r.*exp(-1i*2*pi*fp*(1:length(x)));
+end
+
+z = filter(hr,1,r);
 
 end
 
--- main.m
+++ main.m
@@ -3,98 +3,140 @@ clear
 
 %% Variables
 
-Ts = 20; %Durée d'un symbole.
+Ts = 12; %Nb d'echantillon d'un symbole.
 nb_bits = 100; %Longueur de la trame binaire.
-%--------------------------------------------
+fe = 10^4;  %Frequence d'echantillonage.
+%--------------------------------------------------------------------------
 V = 1;
 bits = V*(randi(2,1,nb_bits)-1); 
-%--------------------------------------------
-SNR = 1000;
-bruit = false; % activation du bruit
-%--------------------------------------------
-filtre = 4; %(1, 2, 3, 4 ) Ordre question 
-%--------------------------------------------
-fp = 0;
-%--------------------------------------------
-switch (filtre)
-    case 1
-        span = 0;
-        h = ones(1,Ts);
-        hr = h;
-        t0 = Ts;
-    case 2
-        span = 0;
-        h = ones(1,Ts);
-        hr = kron([1 0], ones(1,Ts/2));
-        t0 = Ts;
-    case 3
-        span = 0;
-        h = [-1*ones(1,Ts/4) ones(1,Ts/2) -1*ones(1,Ts/4)];
-        hr = h;
-        t0 = Ts;
-    case 4
-        span = 10;
-        h = rcosdesign(0.5,span,Ts);
-        hr = h;
-        t0 = span*Ts+1; %Instant initial.
+%--------------------------------------------------------------------------
+SNR = 10^(2/10);
+bruit = 'none'; %('none','real','complex') type de bruit dans le canal
+%--------------------------------------------------------------------------
+filtre = 'RA'; %('RA','RNA','FA','CA') filtre Forme+Reception
+%--------------------------------------------------------------------------
+%Instant initial.
+if strcmp(filtre,'CA')
+    span = 10;
+    t0 = span*Ts+1;
+else
+    t0 = Ts;
 end
+%--------------------------------------------------------------------------
+% fp = 3000/fe;   
+fp = 0;
+%--------------------------------------------------------------------------
+modu = 'BPSK'; %Type de modulation (BPSK,QPSK,8PSK,16QAM) 
 
 %% Chaine de transmission
 
-[ x, z, ~, ~ ] = chaine_transmission(bits, Ts, h, hr, SNR, bruit, fp );
+[ x, z ] = chaine_transmission(bits, Ts, filtre, SNR, bruit, fp, modu);
 
-%% Echantillonage
+%% Echantillonnage
 
 z_echan = z(t0:Ts:end);
 
+%% Demapping
 
-%% Calcul du TEB
+bits_estimes = zeros(1,nb_bits);
+switch(modu)
+    case 'BPSK'
+        bits_estimes = (z_echan > 0);
+    case 'QPSK'
+        bits_estimes(1:2:end) = (real(z_echan) > 0 );
+        bits_estimes(2:2:end) = (imag(z_echan) > 0 );
+    case '8PSK'
+        bits_recus2 = pskdemod(z_echan, 8, 0, 'gray');
+        bits_recus1 = de2bi(bits_recus2)';
+        bits_estimes = bits_recus1(:)';
+    case '16QAM'
+        bits_recus2 = qamdemod(z_echan, 16)';  
+        bits_recus1 = de2bi(bits_recus2)';
+        bits_estimes = bits_recus1(:)';
+end
 
-TEB = sum((z_echan > 0) ~= bits(1:end-span)) / nb_bits;
+TEB = sum(bits_estimes ~= bits) / nb_bits;
+
+%% Enveloppe complexe sur frequence
+
+xp = x.*exp(1i*2*pi*fp*(1:length(x)));
 
 %% Affichages
+%--------------------------------------------------------------------------
+% Phase
 
-figure(1)
+% figure(1)
+% plot(real(x), 'black')
+% grid on
+% title("Trace du signal en phase")
+% xlabel("Temps")
+% ylabel("Amplitude")
 
-% Symboles transmis
+%--------------------------------------------------------------------------
+% Quadrature
+%--------------------------------------------------------------------------
 
-plot(x, 'black')
-grid on
-title("Trace du signal d'emision")
-%-------------------------------------------------
-figure (2)
+% figure(2)
+% plot(imag(x), 'blue')
+% grid on
+% title("Trace du signal en quadrature")
+% xlabel("Temps")
+% ylabel("Amplitude")
 
-plot(z, 'red')
-grid on;
-title("Trace du signal de reception")
+%--------------------------------------------------------------------------
+% Porteuse
+%--------------------------------------------------------------------------
 
-%-------------------------------------------------
-figure(3)
+% figure (3)
+% plot(real(xp), 'red')
+% grid on;
+% title("Trace du signal sur frequence porteuse")
+% xlabel("Temps")
+% ylabel("Amplitude")
 
+%--------------------------------------------------------------------------
 % Diagramme de l'oeil
+%--------------------------------------------------------------------------
+
+figure(4)
 hold on
-%axis([2 2*Ts -2 2]);
 for i=1:nb_bits/2
-    plot(z(i*Ts:(i+2)*Ts))
+    plot((0:2*Ts),z(i*Ts:(i+2)*Ts))
 end
 hold off
 title("Diagramme de l'oeil")
 
-%-------------------------------------------------
+%--------------------------------------------------------------------------
+% Constellation en sortie d'echantillonneur
+%--------------------------------------------------------------------------
 
-figure (4)
+% figure(5)
+% if strcmp(modu,'BPSK')
+%     plot(complex(z_echan,0),'*','LineStyle','none');
+% else
+%     plot(z_echan,'*','LineStyle','none');
+% end
+% grid on;
+% title("Constellation");
+% xlabel("Reel")
+% ylabel("Imaginaire")
 
-hhr = conv(h,hr);
-% forme de h(t) * hr(t)
-grid on
-plot(hhr, 'red')
-title("Réponse impulsionnelle de h(t) * hr(t)")
+%--------------------------------------------------------------------------
+% DSP
+%--------------------------------------------------------------------------
 
-%-------------------------------------------------
+% figure(6)
+% frqs = (-fe:fe-1)/fe;
+% plot(frqs,abs(fft(xp,length(frqs))).^2);
+% title("DSP du signal sur frequence porteuse");
+% xlabel("Frequences")
+% ylabel("Amplitude")
+
+%--------------------------------------------------------------------------
+% Valeurs puissance + TEB
+%--------------------------------------------------------------------------
+
+% fprintf('Valeur de la puissance = %1.2f\n',mean(abs(real(xp)).^2));
+% fprintf('Valeur de la puissance = %1.2f\n',mean(abs(xp).^2));
 
 fprintf('Valeur du TEB = %1.2f\n',TEB)
-
-%-------------------------------------------------
-
-
-
